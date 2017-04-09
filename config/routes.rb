49ad34648ac10631cfa9cff5b1bc163955622235
Hukui <tckuikui1234@gmail.com>
2017-04-10 06:50:30 +0800

    user can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,29 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
+
   namespace :admin do
     resources :products
   end
+
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
   resources :carts do
     collection do
       delete :clean
       post :checkout
     end
   end
+
   resources :cart_items
+
   resources :orders
+
+  namespace :account do
+    resources :orders
+  end
 end
