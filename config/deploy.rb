5dfdacad8c2435ebdeca3d479498576e8c7b5587
Hukui <tckuikui1234@gmail.com>
2017-05-10 11:48:14 +0800

    fix deploy.rb code bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock "3.8.1"
 
-set :application, "jd"
-set :repo_url, "git@github.com:Hukui-Reborn/jd.git"
+set :application, "jdtest"
+set :repo_url, "git@github.com:Hukui-Reborn/jdtest.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/apps/jd"
+set :deploy_to, "/home/apps/jdtest"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -22,13 +22,11 @@ set :deploy_to, "/home/apps/jd"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-
-set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-set :linked_files, fetch(:linked_dirs, []).push('log','tmp/cache','tmp/sockets','vender/bundle')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
